02f1b9c8714043dc0f0c88e7931cf1a3f9bb9d14
Sumiza <Sumiza@gmail.com>
2026-01-30 20:03:59 -0600

    typofix

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.10",
-    extra_require = {
+    extras_require = {
         "timer": ["secondstotext"],
         "discord": ["requests"],
         "async": ["httpx"],
